3ee010a1ab93cf812872d2f3e59c30a104ed86ca
Sukumar Varma <s.varmagt@gmail.com>
2019-11-06 19:36:46 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:dexter/signinform/Loginpage.dart';
+import 'package:dexter/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.grey,
+      ),
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.onAuthStateChanged,
+      builder: (context,AsyncSnapshot<FirebaseUser> snapshot) {
+        // if(snapshot.connectionState == ConnectionState.waiting)
+        //   return SplashPage();
+        if(!snapshot.hasData || snapshot.data == null)
+          return MyApp1();
+        return HomePage();
+      },
+    );
+  }
+}
